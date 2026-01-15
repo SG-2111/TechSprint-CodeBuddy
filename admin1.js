93f83eec3d8bd9ed17da6cb9a3eb709f94165756
Sanjana <sanjana.gokulnath@gmail.com>
2026-01-15 17:32:11 +0530

    Added env track

--- admin1.js
+++ admin1.js
@@ -1,5 +1,5 @@
 require('dotenv').config({ path: './code.env' });
-
+const projectId = process.env.FB_PROJECT_ID;
 
 const admin = require('firebase-admin');
 
@@ -10,6 +10,7 @@ admin.initializeApp({
     clientEmail: process.env.FB_CLIENT_EMAIL,
     privateKey: process.env.FB_PRIVATE_KEY.replace(/\\n/g, '\n'),
   }),
+  databaseURL:process.env.FB_DATABASE_URL
 });
 
 module.exports = admin;
